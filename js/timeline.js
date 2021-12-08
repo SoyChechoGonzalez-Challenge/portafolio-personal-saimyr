06920e47ef82175e4a304df3acc7796057165923
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2021-12-08 16:30:35 -0500

    Se programa timeline funcional

--- js/timeline.js
+++ js/timeline.js
@@ -1,80 +1,24 @@
-let selectedRow = null;
+// Tomamos el botón del formulario
+let newItem = document.getElementById("newList");
 
-function onFormSubmit() {
-  if (validate()) {
-    let formData = readFormData();
-    if (selectedRow == null) insertNewRecord(formData);
-    else updateRecord(formData);
-    resetForm();
-  }
-}
+// Creamos la función que toma los datos del modal y los incrusta en el article
+const insertNewItem = () => {
 
-function readFormData() {
-  let formData = {};
-  formData["course"] = document.getElementById("course").value;
-  formData["dateFinish"] = document.getElementById("dateFinish").value;
-  formData["university"] = document.getElementById("university").value;
-  return formData;
-}
+  let date = document.getElementById("dateFinish").value;
+  let course = document.getElementById("course").value;
+  let university = document.getElementById("university").value;
+  let article = document.querySelector(".btn-actions");
+  const newList = `
+  <li>
+      <div>
+          <time>${date}</time>
+          <span>${course} - ${university}</span>
+      </div>
+  </li>`;
+  article.insertAdjacentHTML("afterend", newList);
+};
 
-function insertNewRecord(data) {
-  let table = document
-    .getElementById("employeeList")
-    .getElementsByTagName("tbody")[0];
-  let newRow = table.insertRow(table.length);
-  cell1 = newRow.insertCell(0);
-  cell1.innerHTML = data.course;
-  cell2 = newRow.insertCell(1);
-  cell2.innerHTML = data.dateFinish;
-  cell3 = newRow.insertCell(2);
-  cell3.innerHTML = data.university;
-  cell4 = newRow.insertCell(3);
-  cell4.innerHTML = `<a onClick="onDelete(this)"><i class="fas fa-trash"></i></a>`;
-
-  //cell4.innerHTML = `<a onClick="onEdit(this)"><i class="far fa-edit"></i></a>
-  //                   <a onClick="onDelete(this)"><i class="fas fa-trash"></i></a>`;
-
-}
-
-function resetForm() {
-  document.getElementById("course").value = "";
-  document.getElementById("dateFinish").value = "";
-  document.getElementById("university").value = "";
-  selectedRow = null;
-}
-
-function onEdit(td) {
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("course").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("dateFinish").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("university").value = selectedRow.cells[2].innerHTML;
-}
-function updateRecord(formData) {
-  selectedRow.cells[0].innerHTML = formData.course;
-  selectedRow.cells[1].innerHTML = formData.dateFinish;
-  selectedRow.cells[2].innerHTML = formData.university;
-}
-
-function onDelete(td) {
-  if (confirm("Are you sure to delete this record ?")) {
-    row = td.parentElement.parentElement;
-    document.getElementById("employeeList").deleteRow(row.rowIndex);
-    resetForm();
-  }
-}
-function validate() {
-  isValid = true;
-  if (document.getElementById("course").value == "") {
-    isValid = false;
-    document.getElementById("courseValidationError").classList.remove("hide");
-  } else {
-    isValid = true;
-    if (
-      !document
-        .getElementById("courseValidationError")
-        .classList.contains("hide")
-    )
-      document.getElementById("courseValidationError").classList.add("hide");
-  }
-  return isValid;
-}
+// Se añade el evento al botón del formulario
+newItem.addEventListener("click", () => {
+  insertNewItem();
+});
---
+++ js/timeline2.js
@@ -0,0 +1,80 @@
+let selectedRow = null;
+
+function onFormSubmit() {
+  if (validate()) {
+    let formData = readFormData();
+    if (selectedRow == null) insertNewRecord(formData);
+    else updateRecord(formData);
+    resetForm();
+  }
+}
+
+function readFormData() {
+  let formData = {};
+  formData["course"] = document.getElementById("course").value;
+  formData["dateFinish"] = document.getElementById("dateFinish").value;
+  formData["university"] = document.getElementById("university").value;
+  return formData;
+}
+
+function insertNewRecord(data) {
+  let table = document
+    .getElementById("employeeList")
+    .getElementsByTagName("tbody")[0];
+  let newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.course;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.dateFinish;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.university;
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = `<a onClick="onDelete(this)"><i class="fas fa-trash"></i></a>`;
+
+  //cell4.innerHTML = `<a onClick="onEdit(this)"><i class="far fa-edit"></i></a>
+  //                   <a onClick="onDelete(this)"><i class="fas fa-trash"></i></a>`;
+
+}
+
+function resetForm() {
+  document.getElementById("course").value = "";
+  document.getElementById("dateFinish").value = "";
+  document.getElementById("university").value = "";
+  selectedRow = null;
+}
+
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("course").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("dateFinish").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("university").value = selectedRow.cells[2].innerHTML;
+}
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.course;
+  selectedRow.cells[1].innerHTML = formData.dateFinish;
+  selectedRow.cells[2].innerHTML = formData.university;
+}
+
+function onDelete(td) {
+  if (confirm("Are you sure to delete this record ?")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("employeeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
+}
+function validate() {
+  isValid = true;
+  if (document.getElementById("course").value == "") {
+    isValid = false;
+    document.getElementById("courseValidationError").classList.remove("hide");
+  } else {
+    isValid = true;
+    if (
+      !document
+        .getElementById("courseValidationError")
+        .classList.contains("hide")
+    )
+      document.getElementById("courseValidationError").classList.add("hide");
+  }
+  return isValid;
+}
